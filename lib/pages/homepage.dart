265dce0e48faeb73b36227e7af0bf211a22cb027
Charan R <charanravi.online@gmail.com>
2021-07-15 17:13:31 +0530

    added cards and carousels

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   // const HomePage({Key? key}) : super(key: key);
@@ -25,77 +26,164 @@ class _HomePageState extends State<HomePage> {
         title: Text('GetNews'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            //Search Wala Container
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              //Search Bar Container
 
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-            decoration: BoxDecoration(
-                color: Colors.black12, borderRadius: BorderRadius.circular(24)),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    print("Search Me");
-                  },
-                  child: Container(
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.blueAccent,
-                    ),
-                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    textInputAction: TextInputAction.search,
-                    onSubmitted: (value) {
-                      print(value);
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(24)),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print("Search Me");
                     },
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: "Search "),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 50,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: navBarItem.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    print(navBarItem[index]);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    margin: EdgeInsets.symmetric(horizontal: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.blueAccent,
+                      ),
+                      margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                     ),
-                    child: Center(
-                      child: Text(
-                        navBarItem[index],
-                        style: TextStyle(
-                          fontSize: 19,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      textInputAction: TextInputAction.search,
+                      onSubmitted: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "Search "),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: navBarItem.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      print(navBarItem[index]);
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Center(
+                        child: Text(
+                          navBarItem[index],
+                          style: TextStyle(
+                            fontSize: 19,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200,
+                autoPlay: true,
+                enlargeCenterPage: true,
+              ),
+              items: items.map((item) {
+                return Builder(builder: (BuildContext context) {
+                  return InkWell(
+                    onTap: () {
+                      print('banner');
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+                      child: Text(item),
+                    ),
+                  );
+                });
+              }).toList(),
+              // options: options
+            ),
+            Container(
+              child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        elevation: 4.0,
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset("images/breakingnews.jpg")),
+                            Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        Colors.black,
+                                        Colors.black.withOpacity(0),
+                                      ],
+                                      // begin: Alignment.topCenter,
+                                      // end: Alignment.bottomCenter,
+                                    ),
+                                  ),
+                                  padding: EdgeInsets.fromLTRB(
+                                      15, 15, 10, 8),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'News Headline',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'new new newnwe ewnwnew',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  final List items = ['slide 1', 'slide 2', 'slide 3', 'slide 4'];
 }
